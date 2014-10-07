a581c8a712ab69847ea802108a11c7899eb39b0d
Jake <jakesyl@gmail.com>
2014-10-07 08:16:08 -0400

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,3 +1,11 @@
 from random import randint
 
 #Add Classes for Game Pieces
+class checker:
+  def __init__:
+    self.pos = [pos[0],pos[1]]# its a list x and y pos based on id
+  def move():
+    
+class game:
+  def check_win(self, pos, uid):
+    
