daf1f4064f9eec7b61bf189e4c0a57ee7bcdaac6
zachpanz88 <zachary@panzarino.com>
2014-10-27 18:11:48 -0400

    Updated Classes

--- Game.py
+++ Game.py
@@ -1,29 +1,48 @@
 from random import randint
 
-#Add Classes for Game Pieces
+#Classes for Game Pieces
 class checker(object):
-  def __init__(self,x,y):
+  def __init__(self,team,x,y):
+    self.team = team
     self.x = x
     self.y = y
   def move(move):
     y = y+1
     if move == "left":
       x = x+1
+        if x > 8:
+          x = x-1
+          print ("Incorrect Move. Loss of Turn")
     elif move == "right":
       x = x-1
+        if x < 0:
+          x = x + 1
+          print ("Incorrect Move. Loss of Turn")
     else:
       print ("Incorrect Move. Loss of Turn")
 class king(checker):
   def move(up,side):
     if up == "up":
       y = y+1
+      if y > 8:
+          y = y-1
+          print ("Incorrect Move. Loss of Turn")
     elif up == "down":
       y = y-1
+      if y < 0:
+          y = y+1
+          print ("Incorrect Move. Loss of Turn")
     else:
       print ("Incorrect Move. Loss of Turn")
     if move == "left":
       x = x+1
+      if x > 8:
+          x = x-1
+          print ("Incorrect Move. Loss of Turn")
     elif move == "right":
       x = x-1
+      if x < 0:
+          x = x+1
+          print ("Incorrect Move. Loss of Turn")
     else:
       print ("Incorrect Move. Loss of Turn")
