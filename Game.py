6a736cf293645fc12b73579a5deb4fc5e9bf5996
zachpanz88 <zachary@panzarino.com>
2014-10-27 18:33:27 -0400

    Added Checkers

--- Game.py
+++ Game.py
@@ -50,3 +50,34 @@ def check_king(checker):
    if checker.y > 8:
      checker = king(checker.team, checker.x, checker.y)
      checker.y = checker.y - 1
+
+#Adding Checkers
+red1 = checker("red", 2, 1)
+red2 = checker("red", 4, 1)
+red3 = checker("red", 6, 1)
+red4 = checker("red", 8, 1)
+red5 = checker("red", 1, 2)
+red6 = checker("red", 3, 2)
+red7 = checker("red", 5, 2)
+red8 = checker("red", 7, 2)
+red9 = checker("red", 2, 3)
+red10 = checker("red", 4, 3)
+red11 = checker("red", 6, 3)
+red12 = checker("red", 8, 3)
+
+black1 = checker("black", 1, 8)
+black2 = checker("black", 3, 8)
+black3 = checker("black", 5, 8)
+black4 = checker("black", 7, 8)
+black5 = checker("black", 2, 7)
+black6 = checker("black", 4, 7)
+black7 = checker("black", 6, 7)
+black8 = checker("black", 8, 7)
+black9 = checker("black", 1, 6)
+black10 = checker("black", 3, 6)
+black11 = checker("black", 5, 6)
+black12 = checker("black", 7, 6)
+
+#Giving Checkers to Each Player
+user_checkers = [red1, red2, red3, red4, red5, red6, red7, red8, red9, red10, red11, red12]
+cpu_checkers = [black1, black2, black3, black4, black5, black6, black7, black8, black9, black10, black11, black12]
